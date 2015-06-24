b414066a48105fa88a50238fafbac40fc57c9979
Guest User <Guest@c-02-05-lab.mddn.vuw.ac.nz>
2015-06-24 13:21:27 +1200

    security edit

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',       # required
-    :aws_access_key_id      => 'AKIAJ2NLE3TGDXOESTGQ',       # required
-    :aws_secret_access_key  => '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',       # required
+    :aws_access_key_id      => 'xxx',       # required
+    :aws_secret_access_key  => 'yyy',       # required
 #:region                 => 'us-east-1'  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'wellingtonexposure'                     # required
