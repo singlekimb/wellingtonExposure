c750ba8fe90198f9fa2e2ffdb5aaa11a15dfa52a
Guest User <Guest@c-02-05-lab.mddn.vuw.ac.nz>
2015-06-24 14:17:30 +1200

    security with s3

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',       # required
-    :aws_access_key_id      => 'xxx',       # required
-    :aws_secret_access_key  => 'yyy',       # required
+    :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],      # required
+    :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],  # required
 #:region                 => 'us-east-1'  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'wellingtonexposure'                     # required
